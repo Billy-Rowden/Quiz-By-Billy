0151e9623d53bdb3a9a92db1e2814dd0bbcf5dad
Billy Rowden <williampsr.rowden@gmail.com>
2024-01-02 23:47:18 +0000

    moved files around in repo to follow structure and naming conventions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -46,7 +46,7 @@ function checkAnswer(index) {
   const currentQuestion = questions[currentQuestionIndex];
   const correctAnswer = currentQuestion.answer;
   const correctSound = new Audio('./assets/sfx/correct.wav');
-  const incorrectSound = new Audio ('./assets/sfx/incorrect.wav')
+  const incorrectSound = new Audio('./assets/sfx/incorrect.wav')
 
   if (index === correctAnswer) {
     score += 10;
