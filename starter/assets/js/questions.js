eb3533393cbd541a047113e58bb81f33de3d5f87
Billy Rowden <williampsr.rowden@gmail.com>
2024-01-02 17:56:02 +0000

    added js files and array of objects to questions.js with questions to later populate with my quiz questions

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+        title: 'Question 1',
+        choices: ['Choice A', 'Choice B', 'Choice C', 'Choice D'], // Array containing the possible answers
+        answer: 0, // Index representing the correct anwer in the array of choices
+    },
+    {
+        title: 'Question 2',
+        choices: ['Choice A', 'Choice B', 'Choice C', 'Choice D'],
+        answer: 0,
+    },
+    {
+        title: 'Question 3',
+        choices: ['Choice A', 'Choice B', 'Choice C', 'Choice D'],
+        answer: 0,
+    },
+    {
+        title: 'Question 4',
+        choices: ['Choice A', 'Choice B', 'Choice C', 'Choice D'],
+        answer: 0,
+    },
+    {
+        title: 'Question 5',
+        choices: ['Choice A', 'Choice B', 'Choice C', 'Choice D'],
+        answer: 0,
+    },
+    {
+        title: 'Question 6',
+        choices: ['Choice A', 'Choice B', 'Choice C', 'Choice D'],
+        answer: 0,
+    },
+    {
+        title: 'Question 7',
+        choices: ['Choice A', 'Choice B', 'Choice C', 'Choice D'],
+        answer: 0,
+    },
+    {
+        title: 'Question 8',
+        choices: ['Choice A', 'Choice B', 'Choice C', 'Choice D'],
+        answer: 0,
+    },
+    {
+        title: 'Question 9',
+        choices: ['Choice A', 'Choice B', 'Choice C', 'Choice D'],
+        answer: 0,
+    },
+    {
+        title: 'Question 10',
+        choices: ['Choice A', 'Choice B', 'Choice C', 'Choice D'],
+        answer: 0,
+    },
+];
